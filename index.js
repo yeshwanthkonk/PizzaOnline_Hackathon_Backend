eefa408807a7f0c4c3583ff9152b6ec1720483bb
yeshwanth konka <Yeshwanth Konka>
2021-01-10 13:59:58 +0530

    till payment API

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongodb = require("mongodb")
 const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken')
 const cors = require("cors")
+const randomstring = require("randomstring")
 require('dotenv').config()
+// const { Razorpay } = require("razorpay")
+// console.log(Razorpay)
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,8 +16,15 @@ const mongodb_url = "mongodb://127.0.0.1:27017/"; //process.env.mongo_url;
 const db = "pizza_order_hackathon";
 const { Authorize, Edit_check, Delete_check } = require("./auth_lib/authorize")
 
+var {transporter, reset_mail_detail, reset_email_template, activate_mail_detail, activate_email_template} = require("./mail_module/send_reset_mail")
+
 app.use(express.json());
 app.use(cors());
+
+// let payment = new Razorpay({
+//     key_id:process.env.key_id,
+//     key_secret:process.env.key_secret,
+// })
 
 app.post("/new_user", async (req, res)=>{
     let data = req.body;
@@ -22,6 +32,7 @@ app.post("/new_user", async (req, res)=>{
     let hash = await bcrypt.hash(data["password"], salt);
     data["password"] = hash;
     data["is_active"] = true;
+    data["verified"] = false;
     try {
         let client  = await mongoClient.connect(mongodb_url);
         let collection = client.db(db).collection('users');
@@ -56,6 +67,9 @@ app.post("/login", async (req, res)=>{
         if(!isValid){
             return res.status(401).json({"detail": "Invalid Credentials"});
         }
+        if(!result[0]["verified"]){
+            return res.status(403).json({"detail": "User Not Verified, Please Check your mail for link"});
+        }
         let token = await jwt.sign({"user_id": result[0]["_id"], "email": result[0]["email"]}, process.env.CODE, {expiresIn: "1h"})
         if(token)
             return res.status(200).json({"detail": "Success", "token": token})
@@ -81,6 +95,150 @@ app.get("/check_status", Authorize ,async (req, res)=>{
     catch(error){
         return res.status(403).json({"detail": "Token Expired"})
     }
+})
+
+app.post("/reset_link", async (req, res)=>{
+    let data = req.body;
+    if(Object.keys(data).length == 0)
+        return res.status(400).json({"detail": "Invalid Body Request"})
+    try{
+        let client  = await mongoClient.connect(mongodb_url);
+        let collection = client.db(db).collection('users');
+        let result = await collection.find({"email": data["email"]}).toArray();
+        if(result.length == 0){
+            return res.status(400).json({"detail": "Email Not Register with us"})
+        }
+        collection = client.db(db).collection('email_links');
+        let random_string = randomstring.generate(64);
+        let response = await collection.findOneAndUpdate({"email": data["email"]},{$set:{"link_id": random_string}});
+        reset_mail_detail['to'] = data["email"]
+        reset_mail_detail['html'] = reset_email_template(random_string)
+        if(!response['lastErrorObject']['updatedExisting']){
+            data['link_id'] = random_string
+            response = await collection.insertOne(data);
+            if(response['insertedCount'] == 1){
+                await transporter.sendMail(reset_mail_detail);
+                client.close();
+                return res.status(200).json({"detail":"New Email link Sent"})
+            }
+            throw "Server Error";
+        }
+        client.close();
+        await transporter.sendMail(reset_mail_detail);
+        return res.status(200).json({"detail":"Updated Email link Sent"})
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({"detail": "Some Error Occured"})
+    }
+})
+
+app.get("/reset_link/:id", async (req, res)=>{
+    let data = {"link_id": req.params["id"]};
+    let client  = await mongoClient.connect(mongodb_url);
+    let collection = client.db(db).collection('email_links');
+    let result = await collection.find(data).toArray();
+    client.close();
+    if(result.length == 0){
+        return res.redirect(process.env.frontend_host+"forgot_password?q='Invalid Link'")
+    }
+    else{
+        return res.redirect(process.env.frontend_host+`reset_password?id=${data["link_id"]}`)
+    }
+})
+
+app.post("/activate_link", async (req, res)=>{
+    let data = req.body;
+    if(Object.keys(data).length == 0)
+        return res.status(400).json({"detail": "Invalid Body Request"})
+    try{
+        let client  = await mongoClient.connect(mongodb_url);
+        let collection = client.db(db).collection('users');
+        let result = await collection.find({"email": data["email"]}).toArray();
+        if(result.length == 0){
+            return res.status(400).json({"detail": "Email Not Register with us"})
+        }
+        collection = client.db(db).collection('email_links');
+        let random_string = randomstring.generate(64);
+        let response = await collection.findOneAndUpdate({"email": data["email"]},{$set:{"link_id": random_string}});
+        activate_mail_detail['to'] = data["email"]
+        activate_mail_detail['html'] = activate_email_template(random_string)
+        if(!response['lastErrorObject']['updatedExisting']){
+            data['link_id'] = random_string
+            response = await collection.insertOne(data);
+            if(response['insertedCount'] == 1){
+                await transporter.sendMail(activate_mail_detail);
+                client.close();
+                return res.status(200).json({"detail":"New Email link Sent"})
+            }
+            throw "Server Error";
+        }
+        client.close();
+        await transporter.sendMail(activate_mail_detail);
+        return res.status(200).json({"detail":"Updated Email link Sent"})
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({"detail": "Some Error Occured"})
+    }
+})
+
+app.get("/activate_link/:id", async (req, res)=>{
+    let data = {"link_id": req.params["id"]};
+    let client  = await mongoClient.connect(mongodb_url);
+    let collection = client.db(db).collection('email_links');
+    let result = await collection.find(data).toArray();
+    if(result.length == 0){
+        client.close();
+        return res.redirect(process.env.frontend_host+"forgot_password?q='Invalid Link'")
+    }
+    else{
+        await collection.deleteOne(data);
+        collection = client.db(db).collection('users');
+        await collection.findOneAndUpdate({"email": result[0]["email"]},{$set:{"verified": true}});
+        client.close();
+        return res.redirect(process.env.frontend_host+`home?id=${data["link_id"]}`)
+    }
+})
+
+app.post("/update_password", async (req, res)=>{
+    let data = req.body["data"];
+    let id = {"link_id": req.body["id"]};
+    try{
+        let client  = await mongoClient.connect(mongodb_url);
+        let collection = client.db(db).collection('email_links');
+        let result = await collection.find(id).toArray();
+        if(result.length == 0){
+            client.close();
+            return res.status(400).json({"detail": "Invalid Link request new link"})
+        }
+        data['email'] = result[0]['email'];
+        let salt = await bcrypt.genSalt(10);
+        let hash = await bcrypt.hash(data["password"], salt);
+        data["password"] = hash;
+        await collection.deleteOne(id);
+        collection = client.db(db).collection('users');
+        let response = await collection.findOneAndUpdate({"email": data["email"]},{$set:{"password": data["password"], "verified": true}});
+        if(!response['lastErrorObject']['updatedExisting']){
+            return res.status(500).json({"detail": "Something Went Wrong"})
+        }
+        return res.status(200).json({"detail": "Success"})
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({"detail": "Something Went Wrong"})
+    }
+})
+
+app.get("/pizza_list", async (req, res)=>{
+    let client  = await mongoClient.connect(mongodb_url);
+    let collection = client.db(db).collection('pizzas');
+    let result = await collection.find().toArray();
+    return res.status(200).json(result);
+})
+
+app.post("/payment", async (req, res)=>{
+    payment.orders.create()
 })
 
 app.listen(port, ()=>console.log(`Server Started on Port-${port}`))
---
+++ mail_module/send_reset_mail.js
@@ -0,0 +1,53 @@
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'yeshwanth.apis@gmail.com',
+      pass: process.env.email_token
+    }
+});
+
+var reset_mail_detail = {
+  from: 'yeshwanth.apis@gmail.com',
+  subject: 'Password Reset Link',
+};
+
+var activate_mail_detail = {
+    from: 'yeshwanth.apis@gmail.com',
+    subject: 'Link to Activate, Hack Pizza',
+  };
+
+function reset_email_template(token){
+    let template = `
+    <table style="width:100%; font-size:50px;">
+        <tr align="center">
+            <td>
+                <img src="${process.env.frontend_host}logo.png" height="200px" width="200px">
+                <div>Reset Password Link.</div>
+                <a href="${process.env.reset_host}reset_link/${token}" target="_blank">Click Here</a>
+                <div>To reset your password for our site</div>
+            </td>
+        </tr>
+    </table>
+    `
+    return template;
+}
+
+function activate_email_template(token){
+    let template = `
+    <table style="width:100%; font-size:50px;">
+        <tr align="center">
+            <td>
+                <img src="${process.env.frontend_host}logo.png" height="200px" width="200px">
+                <div>Account Activation Link</div>
+                <a href="${process.env.reset_host}activate_link/${token}" target="_blank">Click Here</a>
+                <div>To activate account for Hack Pizza</div>
+            </td>
+        </tr>
+    </table>
+    `
+    return template;
+}
+
+module.exports = {transporter, reset_mail_detail, reset_email_template, activate_mail_detail, activate_email_template}
